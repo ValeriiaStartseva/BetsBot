b48169ab9c9bd566c8f653d581ad7f25d812279c
valeriiastartseva <kaleriiastartseva505@gmail.com>
2023-01-31 17:23:03 +0200

    created second acc

--- mails/config.py
+++ mails/config.py
@@ -1,8 +1,8 @@
 ENCODING = "utf-8"
-USERNAME1 = "dovbanute@rambler.ru"
+USERNAME1 = "dovbanute@rambler.ru"  # UserMain in Expari
 MAIL_PASS1 = "2HX7wZgM"
 
-USERNAME2 = "dovbanute1@rambler.ru"
+USERNAME2 = "dovbanute1@rambler.ru"         # User2 in Expari
 MAIL_PASS2 = "2HX7wZgN"
 
 
