ed761d33bb89c40b2ef05827500fa9cd0dddd8cf
LightOfTheSun <vladimirov.ese@gmail.com>
2023-02-08 21:01:19 +0200

    добавил исправление, чтобы читало все непрочитанные письма в одном вызове (сейчас читало только первое непрочитанное)

--- mails/get_new_emails.py
+++ mails/get_new_emails.py
@@ -5,31 +5,33 @@ from mails.config import ENCODING
 from mails.helpers import get_letter_text, connection, get_link
 
 
-def get_new_emails():
+def get_new_emails() -> list[dict[str, str]]:
     imap = connection()
     if not imap:
-        return
+        return []
 
     status, messages = imap.select("INBOX")  # папка входящие
-    res, unseen_msg = imap.uid("search", "UNSEEN", "ALL")
-    unseen_msg = unseen_msg[0].decode(ENCODING).split(" ")
+    res, unseen_msgs = imap.uid("search", "UNSEEN", "ALL")
+
     information = []
-    if unseen_msg[0]:
-        for letter in unseen_msg:
-            res, msg = imap.uid("fetch", letter, "(RFC822)")
-            if res == "OK":
-                msg = email.message_from_bytes(msg[0][1])
-                letter_text = get_letter_text(msg)
-                letter_links = get_link(msg)
-                information.append({
-                    'text': letter_text,
-                    'link': letter_links,
-                })
-                return information
-        imap.logout()
-    else:
-        imap.logout()
-        return
+    for unseen_msg in unseen_msgs:
+        unseen_msg = unseen_msg.decode(ENCODING).split(" ")
+        if unseen_msg:
+            for letter in unseen_msg:
+                res, msg = imap.uid("fetch", letter, "(RFC822)")
+                if res == "OK":
+                    msg = email.message_from_bytes(msg[0][1])
+                    letter_text = get_letter_text(msg)
+                    letter_links = get_link(msg)
+                    information.append({
+                        'text': letter_text,
+                        'link': letter_links,
+                    })
+        else:
+            continue
+
+    imap.logout()
+    return information
 
 
 
