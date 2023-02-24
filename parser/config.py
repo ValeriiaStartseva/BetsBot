e5791848066d00c83e0d2361fc87631d6bc7d11f
valeriiastartseva <kaleriiastartseva505@gmail.com>
2023-02-24 13:10:26 +0200

    created headers_for post request

--- parser/config.py
+++ parser/config.py
@@ -1,6 +1,21 @@
 headers = {
     'Cookie': '_ga=GA1.2.981950939.1675169280; _ym_uid=1675169280650801792; _ym_d=1675169280; _gid=GA1.2.1768575977.1676986785; _ym_isad=1; SESSION=Nzk5NzY2MDUtZTk2Ny00NzM2LWJhYTktNzYzMzVmODEzYjlj; _gat=1'}
-
+headers_for_post = {'Accept': 'application/json, text/plain, */*', 'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'uk-UA,uk;q=0.9,ru-UA;q=0.8,ru;q=0.7,en-US;q=0.6,en;q=0.5',
+            'Connection': 'keep-alive',
+            'Content-Length': '446',
+            'Content-Type': 'application/json; charset=UTF-8',
+            'Cookie': '_ga=GA1.2.981950939.1675169280; _ym_uid=1675169280650801792; _ym_d=1675169280; _gid=GA1.2.1768575977.1676986785; _ym_isad=1; SESSION=Nzk5NzY2MDUtZTk2Ny00NzM2LWJhYTktNzYzMzVmODEzYjlj; _gat=1',
+            'Host': 'expari.com',
+            'Origin': 'https://expari.com',
+            'Referer': 'https://expari.com/topic/create',
+            'sec-ch-ua': '"Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"',
+            'sec-ch-ua-mobile': '?0',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-origin',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+            'X-CSRF-TOKEN': 'a8c976c9-fd6b-4f2e-b129-5fb9c7a421bc'}
 url_bet = 'https://expari.com/api/topics'
 url_sport = 'https://expari.com/api/bet/sports/'
 url_league = 'https://expari.com/api/bet/leagues/'
--- parser/requests_read.py
+++ parser/requests_read.py
@@ -1,8 +1,8 @@
 import requests
-from parser.config import url_league, url_sport, url_event, url_bet, url_odds, url_odd, headers
+from parser.config import url_league, url_sport, url_event, url_bet, url_odds, url_odd, headers, headers_for_post
 from dict import sport_id, league_id, event_id, odds_id
 from data import name_event, coefficient, MaxValue, displayName
-data = {"id": None, "blog": {"id": 19994}, "title": str(name_event), "content": "", "price": 0,
+json = {"id": None, "blog": {"id": 19994}, "title": str(name_event), "content": "", "price": 0,
         "publish": True, "closeComment": False,
         "bet": {"bookmakerId": 1, "sportId": str(sport_id), "leagueId": str(league_id), "eventId": str(event_id),
                 "oddId": str(odds_id), "value": 0, "isHidden": False, "coefficient": str(coefficient),
@@ -14,8 +14,7 @@ league_choose = requests.get(url_league + str(sport_id) + str('/1'), headers=hea
 event_choose = requests.get(url_event + str(sport_id) + '/' + str(league_id), headers=headers)
 odds_choose = requests.get(url_odds + str(event_id))
 odd_choose = requests.get(url_odd + str(odds_id))
-bet = requests.post(url_bet, data=data, headers=headers)
-
+bet = requests.post(url_bet, json=json, headers=headers_for_post)
 print(sport_choose.status_code)
 print(league_choose.status_code)
 print(event_choose.status_code)
