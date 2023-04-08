00b09c0435008f6d26557ef298ba3ebb35046a54
LightOfTheSun <vladimirov.ese@gmail.com>
2023-04-08 12:22:57 +0300

    improvements for main and asyncio loop

--- main.py
+++ main.py
@@ -10,10 +10,9 @@ from telegram.config import user_id
 from aiogram.utils import executor
 import asyncio
 
-new_topics = []
-
 
 async def email_checker():  # read new emails and add links to the global new_topics
+    new_topics = []
     await bot.send_message(chat_id=user_id, text=f'kindly checking new emails at {dt.today()}')
     try:
         new_topics.extend(get_new_emails())
@@ -22,15 +21,16 @@ async def email_checker():  # read new emails and add links to the global new_to
     return new_topics
 
 
-async def timed_messages_worker(loop):
+async def timed_messages_worker():
+    unprocessed_topics = []
     while True:
         print('test')
         await bot.send_message(chat_id=user_id, text='The program has been started')
-        unprocessed_topics = await email_checker()
+        unprocessed_topics.extend(await email_checker())
 
         if len(unprocessed_topics) == 0:
             await bot.send_message(chat_id=user_id, text='There is not new emails')
-            sleep(360)
+            await asyncio.sleep(360)
             continue
         for i, link in enumerate(unprocessed_topics.copy()):
             try:
@@ -46,18 +46,21 @@ async def timed_messages_worker(loop):
             unprocessed_topics.pop(i)
             await asyncio.sleep(1)
 
-# if __name__ == '__main__':
-#     loop = asyncio.get_event_loop()
-#     loop.create_task(timed_messages_worker())
-#     executor.start_polling(dp)
+
 if __name__ == '__main__':
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     try:
-        asyncio.run(timed_messages_worker(loop=loop))
+        # asyncio.run(timed_messages_worker(loop=loop))
+        task = loop.create_task(timed_messages_worker())
+        print('starting_polling')
         executor.start_polling(dp)
+        loop.run_forever()
     except KeyboardInterrupt:
         pass
+    finally:
+        loop.stop()
+        loop.close()
 
 
 
